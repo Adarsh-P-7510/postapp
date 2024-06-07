5daecc00195c91a89a5c7ee5c90301bb38bd0769
Adarsh-P-7510 <adarshp7510@gmail.com>
2024-06-07 21:19:39 +0530

    created

--- src/App.js
+++ src/App.js
@@ -3,15 +3,20 @@ import './App.css';
 import AddPost from './components/AddPost';
 import PostSearch from './components/PostSearch';
 import DeletePost from './components/DeletePost';
+import ViewAll from './components/ViewAll';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div>
-      <AddPost/>
-      <PostSearch/>
-      <DeletePost/>
-      
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<AddPost/>}/>
+      <Route path='/PostSearch' element={<PostSearch/>}/>
+      <Route path='/DeletePost' element={<DeletePost/>}/>
+      <Route path='/viewAll' element={<ViewAll/>}/>
+    </Routes>
+    </BrowserRouter>
+    
   );
 }
 
