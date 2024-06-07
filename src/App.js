cf7e934df280b56caf5b91e4dc8db1030a5d20c0
Adarsh-P-7510 <adarshp7510@gmail.com>
2024-06-07 20:18:59 +0530

    created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import AddPost from './components/AddPost';
+import PostSearch from './components/PostSearch';
+import DeletePost from './components/DeletePost';
 
 function App() {
   return (
     <div>
       <AddPost/>
+      <PostSearch/>
+      <DeletePost/>
       
     </div>
   );
